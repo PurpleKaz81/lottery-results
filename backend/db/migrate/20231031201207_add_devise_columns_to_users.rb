d44b740951d132899c28bdbb2f4cccfd80983427
rafael kasinski <rakasin@gmail.com>
2023-10-31 17:17:51 -0300

    Add Devise columns to Users table and update .gitignore

---
+++ backend/db/migrate/20231031201207_add_devise_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddDeviseColumnsToUsers < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :encrypted_password, :string
+    add_column :users, :reset_password_token, :string
+    add_column :users, :reset_password_sent_at, :datetime
+  end
+end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_31_194241) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_31_201207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_31_194241) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "encrypted_password"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
